853ea9db06ccf4914af805b6b786504d6a876302
ckhaorus <matsuda.kaoru@m.alhinc.jp>
2020-09-11 01:54:53 +0900

    Login画面 バリデーションメッセージ表示

--- src/main/java/com/example/alhproject/form/LoginForm.java
+++ src/main/java/com/example/alhproject/form/LoginForm.java
@@ -1,6 +1,5 @@
 package com.example.alhproject.form;
 
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 import lombok.Getter;
@@ -9,9 +8,8 @@ import lombok.Setter;
 @Getter
 @Setter
 public class LoginForm {
-	@Pattern(regexp = "[a-zA-Z0-9]*")
-	@Size(min = 6, max = 20)
+	@Size(max = 20)
 	private String loginId;
-	@Size(min = 6, max = 20)
+	@Size(max = 20)
 	private String password;
 }
